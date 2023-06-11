d94dc29f68df9c1df7aa385922a4f6ad96fb8403
erick.bates <erickjosephbates@gmail.com>
2023-06-11 16:03:20 -0400

    More detail to product display.

--- AdvancedJavaProgramming_CourseProject/src/Inventory.java
+++ AdvancedJavaProgramming_CourseProject/src/Inventory.java
@@ -296,7 +296,7 @@ public class Inventory {
 		try {
 			System.out.println(
 					rs.getString("productID") + ". " + rs.getString("productName") + "(" + rs.getString("productType")
-							+ ")" + " | #" + rs.getInt("quantity") + " | $" + rs.getDouble("productPrice"));
+							+ ")" + " | Qyt: #" + rs.getInt("quantity") + " | Price: $" + rs.getDouble("productPrice"));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
